1bec8075c0851e2e8522a577672ff4fb3b906478
gondsachin40 <162681936+gondsachin40@users.noreply.github.com>
2025-04-22 23:27:47 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -45,6 +45,7 @@ exports.loginUser = async (req, res) => {
     // Send token as an HTTP-only cookie
     res.cookie("token", token, {
       httpOnly: true,
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
