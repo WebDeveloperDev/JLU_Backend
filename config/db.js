ddb692b031767839efea217471660116f1d1a372
ShivangiGond01 <gondsachin2006@gmail.com>
2025-12-31 19:37:48 +0530

    hey bro

--- config/db.js
+++ config/db.js
@@ -3,13 +3,10 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error("MongoDB connection error:", error.message);
     process.exit(1);
   }
 };
